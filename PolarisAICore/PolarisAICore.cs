289267c76a7f806c04a67ed48372b0c5544fef21
Kaydon stubbs <k.rydge.stubbs@gmail.com>
2021-04-30 09:56:38 -0600

    Added serilog with consol sink

--- PolarisAICore/PolarisAICore.cs
+++ PolarisAICore/PolarisAICore.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Newtonsoft.Json.Linq;
 using PolarisAICore.Properties;
+using Serilog;
 
 namespace PolarisAICore {
 	public class PolarisAICore {
@@ -12,15 +13,21 @@ namespace PolarisAICore {
             Resources.ResourceManager.GetString("DBpassword"));
 
         static void Main() {
+            ILogger logger = new LoggerConfiguration()
+                .WriteTo.File("../../../logs.txt")
+                .WriteTo.Console()
+                .CreateLogger();
 
+            Log.Logger = logger;
             while (true) {
+                Log.Information("waiting for a request");
                 Console.WriteLine("Enter a test query:");
                 Console.WriteLine(CognizeDebug(Console.ReadLine()));
             }
         }
 
         public static JObject Cognize(String query){
-
+            Log.Information("query is {query}", query);
             Utterance utterance = new Utterance(
                 IntentClassificatorSingleton.Instance.Cognize(query));
             utterance.Response = Response.ResponseController.SetResponse(utterance);
@@ -31,7 +38,7 @@ namespace PolarisAICore {
         }
 
         public static String CognizeDebug(String query) {
-
+            Log.Information("query is {query}", query);
             Utterance utterance = new Utterance(
                 IntentClassificatorSingleton.Instance.Cognize(query));
             utterance.Response = Response.ResponseController.SetResponse(utterance);
